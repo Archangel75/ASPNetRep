ea33a8efd96cd5d048a044c76f8a6a593972ec88
Archangel75 <garrrus@yandex.ru>
2018-01-10 12:07:26 +0300

    Added dbcontext and dbinitializer

--- SimpleBookStore/SimpleBookStore/Models/BookContext.cs
+++ SimpleBookStore/SimpleBookStore/Models/BookContext.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace SimpleBookStore.Models
 {
-    public class BookContext
+    public class BookContext: DbContext
     {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Purchase> Purchases { get; set; }
+
+    }
+
+    public class BookInitializer: DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            context.Books.Add(new Book { Id = 1, Name = "451 градус", Author = "Бредберри", Price = 100 });
+            context.Books.Add(new Book { Id = 2, Name = "Вино из одуванов", Author = "Бредберри", Price = 100 });
+            context.Books.Add(new Book { Id = 3, Name = "Мор ученик смерти", Author = "Бредберри", Price = 100 });
+
+            base.Seed(context);
+        }
     }
 }
