140400031ea92250c68971e138c6babafe5b8052
Archangel75 <garrrus@yandex.ru>
2018-01-12 15:52:48 +0300

    added news,review,categories models and db. edited webconfig

---
+++ MyReviewProject/MyReviewProject/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyReviewProject.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+
+        public string SubjectName { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime DateCreate { get; set; }
+
+        public string Content { get; set; }
+
+        public int Rating { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
