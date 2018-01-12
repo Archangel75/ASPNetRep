fe7c3c1be0778c8c453720ac67d6d5b69f7a4f15
Archangel75 <garrrus@yandex.ru>
2018-01-12 16:01:05 +0300

    Added Subject model.

---
+++ MyReviewProject/MyReviewProject/Models/Subjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyReviewProject.Models
+{
+    public class Subjects
+    {
+        public int SubjectId { get; set; }
+
+        public string Name { get; set; }
+
+        public string CategoryId { get; set; }
+
+        public int AverageRating { get; set; }
+
+
+    }
+}
