a921763e2fe7255dc52862441741363af5b78401
Archangel75 <garrrus@yandex.ru>
2018-01-16 16:54:32 +0300

    Edited sidebar style. Added Users list in index(not work)

--- MyReviewProject/MyReviewProject/Controllers/HomeController.cs
+++ MyReviewProject/MyReviewProject/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using MyReviewProject.Models;
+﻿using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Microsoft.AspNet.Identity;
+using MyReviewProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +9,20 @@ using System.Web.Mvc;
 
 namespace MyReviewProject.Controllers
 {
+    
     public class HomeController : Controller
     {
         ReviewContext db = new ReviewContext();
 
-        public ActionResult Index()
-        {         
-
-            return View(db.Categories);
+        List<string> boopers = new List<string>();
+        
+        
+        
+        public ActionResult Index(ControllerContext context)
+        {
+            ApplicationUser user = new ApplicationUser();
+            ViewBag.users = user.Logins.ToList();
+            return View();
         }
     }
 }
