fb453b17f2013e53248dc3c5db7a96f0b2974bb8
Archangel75 <garrrus@yandex.ru>
2018-01-30 16:54:41 +0300

    Added dropdownboxes with radio inside with nice anims. All colors moved into colors css and remade whole site scheme(in progress)

---
+++ MyReviewProject/MyReviewProject/Scripts/Dropdowncheckboxes.js
@@ -0,0 +1,66 @@
+﻿/*
+	Dropdown with Multiple checkbox select with jQuery - May 27, 2013
+	(c) 2013 @ElmahdiMahmoud
+	license: https://www.opensource.org/licenses/mit-license.php
+*/
+//дропает
+$(".dropdownCat dt a").on("click", function() {
+    $(".dropdownCat dd ul").slideToggle("fast");
+});
+//прячет
+$(".dropdownCat dd ul li a").on("click", function() {
+    $(".dropdownCat dd ul").hide();
+});
+//получает значение
+function getSelectedCatValue(id) {
+  return $("#" + id)
+    .find(".dropdownCat dt a span.value")
+    .html();
+}
+
+$(document).bind("click", function(e) {
+  var $clicked = $(e.target);
+  if (!$clicked.parents().hasClass("dropdownCat")) $(".dropdownCat dd ul").hide();
+});
+
+//Categories
+$('.mutliSelectCat input').on('click', function () {
+        var title = $(this).closest('.mutliSelectCat').find('input').val(),
+            title = $(this).val();
+        $('.multiSelCat span').remove();
+        var html = '<span title="' + title + '">' + title + '</span>';
+        $('.multiSelCat').append(html);
+        $(".hidaCat").hide();
+});
+
+//SubCategories
+//дропает
+$(".dropdownSub dt a").on("click", function () {
+    $(".dropdownSub dd ul").slideToggle("fast");
+});
+//прячет
+$(".dropdownSub dd ul li a").on("click", function () {
+    $(".dropdownSub dd ul").hide();
+});
+//получает значение
+function getSelectedCatValue(id) {
+    return $("#" + id)
+        .find(".dropdownSub dt a span.value")
+        .html();
+}
+//прячет когда кликаем в сторону
+$(document).bind("click", function (e) {
+    var $clicked = $(e.target);
+    if (!$clicked.parents().hasClass("dropdownSub") ) $(".dropdownSub dd ul").hide();
+});
+
+//SubCategories
+$('.mutliSelectSub input').on('click', function () {
+    var title = $(this).closest('.mutliSelectSub').find('input').val(),
+        title = $(this).val();
+    $('.multiSelSub span').remove();
+    var html = '<span title="' + title + '">' + title + '</span>';
+    $('.multiSelSub').append(html);
+    $(".hidaSub").hide();
+});
+
